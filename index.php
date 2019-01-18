674a47e97387a4505895ad5e878e1d9e7c35f73d
VirusS <nguyenvanbinh3790@gmail.com>
2019-01-18 08:50:26 +0700

    init project

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VirusS
+ * Date: 2019/1/18
+ * Time: 8:31
+ */?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/icons.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <title>VirusS Clothing</title>
+</head>
+<body>
+<!--    Begin Header-->
+    <header>
+
+    </header>
+<!--    End Header-->
+<!--    Begin Main-->
+    <main>
+
+    </main>
+<!--    End Main-->
+<!--    Begin Footer-->
+    <footer>
+
+    </footer>
+<!--    End Footer-->
+</body>
+</html>
